ad5b1779641e8b359d01353f2610940ac851342c
Ryan Crawford <RyanCCrawford@live.com>
2019-08-29 12:02:45 -0400

    fix Cofgig

---
+++ config/config.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+
+module.exports = {
+    development: {
+        username: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        host: process.env.MYSQL_HOST,
+        dialect: "mysql",
+        dialectOptions: {
+            dateStrings: true,
+            typeCast: true
+        }
+    },
+    test: {
+        username: process.env.MYSQL_USERNAME || "root",
+        password: process.env.MYSQL_PASSWORD || null,
+        database: process.env.MYSQL_TEST_DATABASE || "testdb",
+        host: process.env.MYSQL_HOST || "localhost",
+        dialect: "mysql",
+        logging: false
+    },
+    production: {
+        // eslint-disable-next-line camelcase
+        use_env_variable: "JAWSDB_URL",
+        dialect: "mysql"
+    }
+};
