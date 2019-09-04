0e5bf6c7f13ce1d232fd343d0c710e9b523d41db
Ryan Crawford <RyanCCrawford@live.com>
2019-09-04 17:19:20 -0400

    Hardware update

--- device/sensor/device-code/__vm/.device-code.vsarduino.h
+++ device/sensor/device-code/__vm/.device-code.vsarduino.h
@@ -6,7 +6,7 @@
 			All non-arduino files created by visual micro and all visual studio project or solution files can be freely deleted and are not required to compile a sketch (do not delete your own code!).
 			Note: debugger breakpoints are stored in '.sln' or '.asln' files, knowledge of last uploaded breakpoints is stored in the upload.vmps.xml file. Both files are required to continue a previous debug session without needing to compile and upload again
 	
-	Hardware: ESP32 Dev Module, Platform=esp32, Package=espressif
+	Hardware: "WeMos" WiFi&Bluetooth Battery, Platform=esp32, Package=espressif
 */
 
 #if defined(_VMICRO_INTELLISENSE)
@@ -19,7 +19,7 @@
 #define HAVE_CONFIG_H
 #define F_CPU 240000000L
 #define ARDUINO 10800
-#define ARDUINO_ESP32_DEV
+#define ARDUINO_Pocket32
 #define ARDUINO_ARCH_ESP32
 #define ESP32
 #define CORE_DEBUG_LEVEL 0
--- device/sensor/device-code/device-code.ino
+++ device/sensor/device-code/device-code.ino
@@ -4,24 +4,25 @@
  Author:	Ryan Crawford
 */
 
-/*
-	Original Code by Rui Santos
-  
-*/
+
+#include <WiFiMQTTManager.h>
+#include <utility.h>
+#include <jwt.h>
+#include <IotWebConf.h>
+#include <EspMQTTClient.h>
+#include <ESP32HTTPUpdateServer.h>
 
 #include <DHTesp.h>
 #include <dummy.h>
 #include <WiFi.h>
 #include "DHT.h"
 
-// Uncomment one of the lines below for whatever DHT sensor type you're using!
-//#define DHTTYPE DHT11   // DHT 11
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-#define DHTTYPE DHT11   // DHT 22  (AM2302), AM2321
 
-// Replace with your network credentials
-const char* ssid = "";  //wifi id
-const char* password = "";  //password
+#define DHTTYPE DHT11
+
+
+const char* ssid = "";
+const char* password = "";
 
 WiFiServer server(80);
 
